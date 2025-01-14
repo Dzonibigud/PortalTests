d684e02d840683c9061dfa5d45e334024330754b
NikolaJankovic <jankovic.kobe@hotmail.com>
2025-01-14 13:30:50 +0100

    Adding portal tests

--- src/test/java/PortalTest1.java
+++ src/test/java/PortalTest1.java
@@ -9,47 +9,17 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
-import javax.swing.*;
 import java.time.Duration;
 
 
 public class PortalTest1 {
         WebDriver driver;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         private static final String BASE_URL = "https://stage.portal.acolin.com/sign-in";
         private static int n = 0;
-
-        @BeforeEach
-        void setUp () {
-            driver = new ChromeDriver();
-
-
-        }
-
-        @AfterEach
-        void tearDown() throws InterruptedException{
-            Thread.sleep(2000);
-            driver.quit();
-        }
-        @Test
-        void openHomePageTest(){
-            driver.get(BASE_URL);
-            driver.manage().window().maximize();
-            String curentUrl = driver.getCurrentUrl();
-            Assertions.assertEquals(BASE_URL, curentUrl);
-
-        }
-        @RepeatedTest(17)
-        void openTabs() throws InterruptedException {
-            n++;
-            System.out.println("Running test number: " + n);
+        public void logIn() throws InterruptedException {
             Actions action = new Actions(driver);
-            WebDriverWait longWait = new WebDriverWait(driver, Duration.ofSeconds(5));
-            driver.get(BASE_URL);
-            driver.manage().window().maximize();
-            WebElement clientPortal = driver.findElement(By.xpath("//h1[text()='Client Portal']"));
-            longWait.until(ExpectedConditions.visibilityOf(clientPortal));
             WebElement emailField = driver.findElement(By.xpath("//input[@name='email']"));
-            action.click(emailField).perform();
             Thread.sleep(1000);
             action.sendKeys(emailField,"nikola.jankovic@acolin.com").perform();
             Thread.sleep(2000);
@@ -59,12 +29,116 @@ public class PortalTest1 {
             WebElement signInButton = driver.findElement(By.xpath("//button[text()='Sign In']"));
             signInButton.click();
             Thread.sleep(4000);
+        }
+
+        @BeforeEach
+        void setUp () {
+            driver = new ChromeDriver();
+        }
+
+        @AfterEach
+        void tearDown() throws InterruptedException{
+            Thread.sleep(2000);
+            driver.quit();
+        }
+        @Test // Testing of just opening the page
+        void openHomePageTest(){
+            driver.get(BASE_URL);
+            driver.manage().window().maximize();
+            String currentUrl = driver.getCurrentUrl();
+            Assertions.assertEquals(BASE_URL, currentUrl);
+
+        }
+        @Test // Testing if it redirects us to the home page after login in
+        void logInTest() throws InterruptedException {
+            driver.get(BASE_URL);
+            driver.manage().window().maximize();
+            logIn();
+            Assertions.assertEquals("https://stage.portal.acolin.com/home", driver.getCurrentUrl());
+        }
+        @Test // Testing if redirects us to sign in page after logging out
+        void logOutTest() throws InterruptedException {
+            driver.get(BASE_URL);
+            driver.manage().window().maximize();
+            logIn();
+            WebElement logOut = driver.findElement(By.xpath("//i[@data-icon='logout-1']"));
+            logOut.click();
+            Assertions.assertEquals("https://stage.portal.acolin.com/sign-in",driver.getCurrentUrl());
+        }
+
+
+        @RepeatedTest(17) // Testing of all tabs that are located on the left side of the page
+        void openTabs() throws InterruptedException {
+            n++;
+            System.out.println("Running test number: " + n);
+            Actions action = new Actions(driver);
+            WebDriverWait longWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            driver.get(BASE_URL);
+            driver.manage().window().maximize();
+            WebElement clientPortal = driver.findElement(By.xpath("//h1[text()='Client Portal']"));
+            longWait.until(ExpectedConditions.visibilityOf(clientPortal));
+            logIn();
             List<WebElement> tabs = driver.findElements(By.xpath("//dd[@class ='acl-menu-list__item']"));
             WebElement test = tabs.get(n-1).findElement(By.xpath(".//a"));
             action.moveToElement(tabs.get(n-1).findElement(By.linkText(tabs.get(n-1).getText()))).click().perform();
             Thread.sleep(2000);
             Assertions.assertTrue(driver.getCurrentUrl().equals(test.getAttribute("href")));
             Thread.sleep(1000);
+        }
+        @Test //Testing of Funds page if there are all named filetrs that should be there(Fund name, ISIN, LifeCycle)
+        void fundsTabTest() throws InterruptedException{
+            boolean check=false;
+            driver.get(BASE_URL);
+            driver.manage().window().maximize();
+            logIn();
+            WebElement funds = driver.findElement(By.xpath("//a[text() = 'Funds']"));
+            funds.click();
+            //Thread.sleep(4000);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class = 'k-cell-inner']")));
+            List<WebElement> filters = driver.findElements(By.xpath("//span[@class = 'k-cell-inner']"));
+            for(WebElement f :filters){
+                System.out.println(f.getText());
+                if(driver.getPageSource().contains(f.getText())){
+                    check=true;
+                }
+            }
+            Assertions.assertTrue(check);
+        }
+        @Test // Testing of Collapse All button and its functionality
+        void fundsCollapseAllTest() throws InterruptedException {
+            driver.get(BASE_URL);
+            driver.manage().window().maximize();
+            logIn();
+            WebElement funds = driver.findElement(By.xpath("//a[text() = 'Funds']"));
+            funds.click();
+            //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tr[@class = 'acl-page-funds__tree-list-isin-row']")));
+            Thread.sleep(2000);
+            WebElement collapseAllButton = driver.findElement(By.xpath("//button[text()= 'Collapse all']"));
+            collapseAllButton.click();
+            Thread.sleep(1000);
+            List<WebElement> shareClass = driver.findElements(By.xpath("//tr[@class = 'acl-page-funds__tree-list-isin-row']"));
+            System.out.println(shareClass.size());
+            Assertions.assertTrue(shareClass.isEmpty());
+        }
+        @Test // Testing of Back To Top button adn its functionality
+        void fundsBackToTopTest() throws InterruptedException {
+            Actions action = new Actions(driver);
+            boolean check = false;
+            driver.get(BASE_URL);
+            driver.manage().window().maximize();
+            logIn();
+            WebElement funds = driver.findElement(By.xpath("//a[text() = 'Funds']"));
+            funds.click();
+            Thread.sleep(2000);
+            WebElement footer = driver.findElement(By.xpath("//footer[@class='acl-footer']"));
+            action.scrollToElement(footer).perform();
+            Thread.sleep(1000);
+            WebElement backToTopButton = driver.findElement(By.xpath("//i[@data-icon = 'arrow-up-1']"));
+            backToTopButton.click();
+            Thread.sleep(3000);
+            WebElement logo = driver.findElement(By.xpath("//a[@title='Acolin Client Portal']"));
+            logo.click();
+            Assertions.assertEquals("https://stage.portal.acolin.com/home", driver.getCurrentUrl());
         }
 }
 
